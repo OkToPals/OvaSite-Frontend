ec5b3b576172286a9208108e194d0c7f125b05b5
Emmanuel <emmco96@gmail.com>
2023-08-10 15:52:03 +0100

    Update hero.js

--- src/components/hero.js
+++ src/components/hero.js
@@ -7,9 +7,9 @@ const Hero = () => {
   <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
     <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
       <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Discover Oversite: 
-        <br className="hidden lg:inline-block" /> Your all-in-one platform for monitoring, evaluation, and data collection
+        <br className="hidden lg:inline-block" /> Your all-in-one platform for data collection,monitoring,and evaluation. 
       </h1>
-      <p className="mb-8 leading-relaxed">Oversite empowers you to effortlessly gather, analyze, and act upon essential information. With a user-friendly interface and powerful tools, Oversite transforms data into actionable intelligence, helping you make informed decisions that drive progress. Step into a new era of efficiency and understanding with Oversite – where monitoring meets innovation.</p>
+      <p className="mb-8 leading-relaxed">Oversite simplifies data collection, analysis, and action. With a user-friendly interface and robust tools, it converts data into actionable insights, enabling informed decisions that fuel progress. Step into a future of efficiency and clarity with Oversite – where monitoring embraces innovation</p>
       <div className="flex justify-center">
         <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Learn more</button>
         <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Get started</button>
