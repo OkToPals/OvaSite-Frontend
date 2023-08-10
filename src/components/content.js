0fe21debd2f122e0386131081bd7c498ffbd78b6
Emmanuel <emmco96@gmail.com>
2023-08-10 17:17:26 +0100

    Update content.js

--- src/components/content.js
+++ src/components/content.js
@@ -7,12 +7,12 @@ const Content = () => {
     <section className="text-gray-600 body-font">
   <div className="container px-5 py-24 mx-auto">
     <div className="flex flex-col text-center w-full mb-20">
-      <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Experience the Power of Oversite: Features that Transform, Benefits that Propel.</h1>
+      <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Features that Transform, Benefits that Propel.</h1>
       <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Experience revolutionary features for gathering, analyzing, and utilizing data. From adaptable forms to real-time insights, Oversite empowers you. Uncover actionable intelligence, streamline collaboration, and drive progress with your most valuable asset—data. Elevate your success with Oversite</p></div>
     <div className="flex flex-wrap">
       <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-        <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">Shooting Stars</h2>
-        <p className="leading-relaxed text-base mb-4">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+        <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">Data Collection and Storage</h2>
+        <p className="leading-relaxed text-base mb-4">Collection and storage of quantitative and qualitative Data from multiple data source while ensuring data security</p>
         <a className="text-indigo-500 inline-flex items-center">Learn More
           <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
             <path d="M5 12h14M12 5l7 7-7 7"></path>
@@ -20,8 +20,8 @@ const Content = () => {
         </a>
       </div>
       <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-        <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">The Catalyzer</h2>
-        <p className="leading-relaxed text-base mb-4">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+        <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">Real-time Data Entry and Updating</h2>
+        <p className="leading-relaxed text-base mb-4">OvaSite facilitates real-time data entry and updating to ensure the most current information is available for analysis</p>
         <a className="text-indigo-500 inline-flex items-center">Learn More
           <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
             <path d="M5 12h14M12 5l7 7-7 7"></path>
@@ -29,8 +29,8 @@ const Content = () => {
         </a>
       </div>
       <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-        <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">Neptune</h2>
-        <p className="leading-relaxed text-base mb-4">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+        <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">Cost-Effective Analysis and Feedback</h2>
+        <p className="leading-relaxed text-base mb-4">After collecting your data, get instant feedback from your analyzed data for informed decision making.</p>
         <a className="text-indigo-500 inline-flex items-center">Learn More
           <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
             <path d="M5 12h14M12 5l7 7-7 7"></path>
@@ -38,8 +38,8 @@ const Content = () => {
         </a>
       </div>
       <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-        <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">Melanchole</h2>
-        <p className="leading-relaxed text-base mb-4">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+        <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">Works Offline</h2>
+        <p className="leading-relaxed text-base mb-4">You can collect data even without an internet connection. Data collected offline, is automatically uploaded once your device is connected to the internet.</p>
         <a className="text-indigo-500 inline-flex items-center">Learn More
           <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
             <path d="M5 12h14M12 5l7 7-7 7"></path>
