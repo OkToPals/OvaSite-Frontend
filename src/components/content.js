4e6a1fca0ecb6616e1f9f26841f0b82293ba60a1
Emmanuel <emmco96@gmail.com>
2023-08-10 17:25:49 +0100

    Update content.js

--- src/components/content.js
+++ src/components/content.js
@@ -13,38 +13,22 @@ const Content = () => {
       <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
         <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">Data Collection and Storage</h2>
         <p className="leading-relaxed text-base mb-4">Collection and storage of quantitative and qualitative Data from multiple data source while ensuring data security</p>
-        <a className="text-indigo-500 inline-flex items-center">Learn More
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
-        </a>
+       
       </div>
       <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
         <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">Real-time Data Entry and Updating</h2>
         <p className="leading-relaxed text-base mb-4">OvaSite facilitates real-time data entry and updating to ensure the most current information is available for analysis</p>
-        <a className="text-indigo-500 inline-flex items-center">Learn More
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
-        </a>
+       
       </div>
       <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
         <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">Cost-Effective Analysis and Feedback</h2>
         <p className="leading-relaxed text-base mb-4">After collecting your data, get instant feedback from your analyzed data for informed decision making.</p>
-        <a className="text-indigo-500 inline-flex items-center">Learn More
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
-        </a>
+       
       </div>
       <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
         <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">Works Offline</h2>
         <p className="leading-relaxed text-base mb-4">You can collect data even without an internet connection. Data collected offline, is automatically uploaded once your device is connected to the internet.</p>
-        <a className="text-indigo-500 inline-flex items-center">Learn More
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
-        </a>
+       
       </div>
     </div>
     <button className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Get Started</button>
