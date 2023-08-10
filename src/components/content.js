afd7d56de7e88c6983c30c7486f89b7db034f5a0
Emmanuel <emmco96@gmail.com>
2023-08-10 17:22:53 +0100

    Update content.js

--- src/components/content.js
+++ src/components/content.js
@@ -47,7 +47,7 @@ const Content = () => {
         </a>
       </div>
     </div>
-    <button className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+    <button className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Get Started</button>
   </div>
 </section>
   )
