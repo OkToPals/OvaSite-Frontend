5d29bc926b423dd26521c1957c7fd4f4dcb57196
KingsleyOnoh38 <kingsleyonoh7@gmail.com>
2023-08-11 02:31:46 +0100

    Update

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Image from 'next/image';
 import Hero from '@/components/hero';
 import Content from '@/components/content';
+// import Cta from '@/components/cta';
 
 export default function Home() {
   const mainStyles = {
-    background: 'radial-gradient(circle, #a6a6a6, #ffffff)',
+    background: '(beige, #CAC0D5)',
     minHeight: '50vh', // Set a minimum height to fill the viewport
     padding: '1rem', // Add padding to the main content using a responsive unit
   };
--- src/components/hero.js
+++ src/components/hero.js
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types';
 
 const Hero = () => {
   return (
-  <section className="text-gray-600 body-font">
-  <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+<section className= "text-gray-600 body-font"  >
+
+<div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
     <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Discover Oversite: 
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium" style={{ color: '#001233' }}>Discover Oversite: 
         <br className="hidden lg:inline-block" /> Your all-in-one platform for data collection,monitoring,and evaluation. 
       </h1>
       <p className="mb-8 leading-relaxed">Oversite simplifies data collection, analysis, and action. With a user-friendly interface and robust tools, it converts data into actionable insights, enabling informed decisions that fuel progress. Step into a future of efficiency and clarity with Oversite – where monitoring embraces innovation</p>
@@ -15,19 +16,19 @@ const Hero = () => {
         <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Get started</button>
       </div>
     </div>
-    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-      <image className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
-    </div>
+      <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+        <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
+      </div>
   </div>
 </section>
   )
 }
 export default Hero;
 
-Hero.propTypes = {
-    imageSrc: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    buttonText1: PropTypes.string.isRequired,
-    buttonText2: PropTypes.string.isRequired,
-};
+// Hero.propTypes = {
+//     imageSrc: PropTypes.string.isRequired,
+//     title: PropTypes.string.isRequired,
+//     description: PropTypes.string.isRequired,
+//     buttonText1: PropTypes.string.isRequired,
+//     buttonText2: PropTypes.string.isRequired,
+// };
