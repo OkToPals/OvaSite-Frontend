826d2558a9c6f79c52f7f9d6473303fb806570ef
KingsleyOnoh38 <kingsleyonoh7@gmail.com>
2023-08-12 10:32:14 +0100

    Updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Image from 'next/image';
 import Hero from '@/components/hero';
 import Content from '@/components/content';
+import Uses from '@/components/uses';
 
 export default function Home() {
   const mainStyles = {
-    background: 'radial-gradient(circle, #a6a6a6, #ffffff)',
+    background: 'Beige-[#DDD0C8]',
     minHeight: '50vh', // Set a minimum height to fill the viewport
     padding: '1rem', // Add padding to the main content using a responsive unit
   };
@@ -13,6 +14,7 @@ export default function Home() {
     <main style={mainStyles}>
       <Hero />
       <Content />
+      <Uses />
     </main>
   );
 }
--- src/components/hero.js
+++ src/components/hero.js
@@ -1,33 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+// import Headerimage from '@components/headerimage.jpg'
 
 const Hero = () => {
   return (
-  <section className="text-gray-600 body-font">
-  <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+<section className= "text-gray-600 body-font"  >
+
+<div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
     <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they sold out
-        <br className="hidden lg:inline-block" />readymade gluten 
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium" style={{ color: '#001233' }}>Discover Oversite: 
+        <br className="hidden lg:inline-block" /> Your all-in-one platform for data collection,monitoring,and evaluation. 
       </h1>
-      <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+      <p className="mb-8 leading-relaxed">Oversite simplifies data collection, analysis, and action. With a user-friendly interface and robust tools,
+       it converts data into actionable insights, 
+       enabling informed decisions that fuel progress. Step into a future of efficiency and clarity with Oversite – where monitoring embraces innovation</p>
       <div className="flex justify-center">
-        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-        <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
+        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Get started</button>
+        <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Learn more</button>
       </div>
     </div>
-    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-      <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
-    </div>
+      <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 border-[#001233] border width-7px">
+        <img className="object-cover object-center rounded" alt="hero" src="/img.png" />
+      </div>
   </div>
 </section>
   )
 }
 export default Hero;
 
-Hero.propTypes = {
-    imageSrc: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    buttonText1: PropTypes.string.isRequired,
-    buttonText2: PropTypes.string.isRequired,
-};
+// Hero.propTypes = {
+//     imageSrc: PropTypes.string.isRequired,
+//     title: PropTypes.string.isRequired,
+//     description: PropTypes.string.isRequired,
+//     buttonText1: PropTypes.string.isRequired,
+//     buttonText2: PropTypes.string.isRequired,
+// };
