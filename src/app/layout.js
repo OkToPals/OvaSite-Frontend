c59dd5cee8eefcaab2b7550c83e573c82deae5ae
KingsleyOnoh38 <kingsleyonoh7@gmail.com>
2023-08-16 09:19:12 +0100

    registration page update

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body classNameName={inter.classNameName}>
+      <body className={inter.className}>
         <Header />
         {children}
         <Footer />
