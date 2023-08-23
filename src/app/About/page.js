40037332390c3680b7b592ffea771992f935aed5
Emmanuel <emmco96@gmail.com>
2023-08-24 00:04:29 +0100

    new

--- src/app/About/page.js
+++ src/app/About/page.js
@@ -8,7 +8,7 @@ export default function About() {
 <div className="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0 mx-auto mt-40 mb-10">
   <h1 className="title-font font-medium text-3xl text-[#001233] mb-4"> About Us: OvaSite - Your Partner in Smart Decision-Making</h1>
   <p className="leading-relaxed text-[#001233] mb-4">
-    Welcome to OvaSite! We're here to help you make smart decisions using the power of information. Our goal is to make it easy for you to collect, store, and understand data, so you can make choices that lead to success.
+    Welcome to OvaSite! We are here to help you make smart decisions using the power of information. Our goal is to make it easy for you to collect, store, and understand data, so you can make choices that lead to success.
   </p>
 </div>
 
@@ -24,7 +24,7 @@ export default function About() {
 <div className="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0 mx-auto mt-10 mb-10">
   <h1 className="title-font font-medium text-3xl text-[#001233] mb-4"> How We Work: Collecting and Keeping Your Data Safe</h1>
   <p className="leading-relaxed text-[#001233] mb-4">
-    At OvaSite, we know that information is super important. We're experts at gathering different types of information from lots of places. Don't worry, your data is safe with us! We use special techniques to make sure no one can see it unless you want them to.
+    At OvaSite, we know that information is super important. We are experts at gathering different types of information from lots of places. Do not worry, your data is safe with us! We use special techniques to make sure no one can see it unless you want them to.
   </p>
 </div>
 
@@ -39,7 +39,7 @@ export default function About() {
 <div className="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0 mx-auto mt-10 mb-10">
   <h1 className="title-font font-medium text-3xl text-[#001233] mb-4"> Get Answers Easily and Quickly: Understand Your Data Better</h1>
   <p className="leading-relaxed text-[#001233] mb-4">
-    Just having data isn't enough; you need to understand what it means. That's where we come in. We help you figure out what your data is saying, so you can make decisions that really work. And the best part? You don't have to wait long to get answers. We show you what your data means right away.
+    Just having data is not enough, you need to understand what it means. That is where we come in. We help you figure out what your data is saying, so you can make decisions that really work. And the best part? You do not have to wait long to get answers. We show you what your data means right away.
   </p>
 </div>
 
@@ -55,7 +55,7 @@ export default function About() {
 <div className="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0 mx-auto mt-10 mb-10">
   <h1 className="title-font font-medium text-3xl text-[#001233] mb-4"> Always Up-to-Date: Get the Latest Info Whenever You Need</h1>
   <p className="leading-relaxed text-[#001233] mb-4">
-  Things change quickly, right? That's why we make sure you always have the newest info at your fingertips. You can put new information into our system anytime, and it will show up right away. This helps you stay on top of things and make good choices based on what's happening right now.
+  Things change quickly, right? That is why we make sure you always have the newest info at your fingertips. You can put new information into our system anytime, and it will show up right away. This helps you stay on top of things and make good choices based on what is happening right now.
   </p>
 </div>
 
@@ -70,7 +70,7 @@ export default function About() {
 <div className="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0 mx-auto mt-10 mb-10">
   <h1 className="title-font font-medium text-3xl text-[#001233] mb-4"> No Internet? No Problem: Collect Data Anywhere</h1>
   <p className="leading-relaxed text-[#001233] mb-4">
-  We know that sometimes you're not connected to the internet. But that shouldn't stop you from collecting data! Our system works even when you're offline. You can gather information wherever you are. When you're back online, your data will automatically go to our safe place.
+  We know that sometimes you are not connected to the internet. But that should not stop you from collecting data! Our system works even when you are offline. You can gather information wherever you are. When you are back online, your data will automatically go to our safe place.
   </p>
 </div>
 
@@ -84,7 +84,7 @@ export default function About() {
 
 <div className="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0 mx-auto mt-10 mb-10">
   <p className="leading-relaxed text-[#001233] mb-4">
-  At OvaSite, we want to give you the tools and knowledge to succeed using data. Our focus is on making data easy to collect, safe to store, and simple to understand. Let's work together to make your decisions the best they can be!
+  At OvaSite, we want to give you the tools and knowledge to succeed using data. Our focus is on making data easy to collect, safe to store, and simple to understand. Let us work together to make your decisions the best they can be!
    </p>
 </div>
 
