a6fba881692dc164cbf77a071ba19bb29691e8bf
Kingsley <117048139+KingsleyOnoh38@users.noreply.github.com>
2023-08-24 18:21:36 +0100

    Update page.js

--- src/app/signin/page.js
+++ src/app/signin/page.js
@@ -16,11 +16,6 @@ export default function SignIn() {
           <h2 className="text-[#001233] text-lg font-medium title-font mb-5 text-center">Sign In</h2>
 
           <div className="relative mb-4 animate__animated animate__fadeIn">
-            <label htmlFor="full-name" className="leading-7 text-sm text-[#001233]">Full Name</label>
-            <input type="text" id="full-name" name="full-name" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-          </div>
-
-          <div className="relative mb-4 animate__animated animate__fadeIn">
             <label htmlFor="email" className="leading-7 text-sm text-[#001233]">Email</label>
             <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
